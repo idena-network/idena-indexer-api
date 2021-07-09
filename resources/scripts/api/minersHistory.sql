bbabed06781685928bdf2667d7f9d42828a4fc4d
mbidenaio <mb@idena.io>
2021-07-09 09:21:55 +0500

    Increase miners history item number

--- resources/scripts/api/minersHistory.sql
+++ resources/scripts/api/minersHistory.sql
@@ -1,5 +1,4 @@
-SELECT coalesce(b.timestamp, 0) "timestamp", mh.online_miners, mh.online_validators
+SELECT mh.block_timestamp, mh.online_miners, mh.online_validators
 FROM miners_history mh
-         LEFT JOIN blocks b on b.height = mh.block_height
-ORDER BY block_height DESC
-LIMIT 500
+ORDER BY mh.block_timestamp DESC
+LIMIT 2000
