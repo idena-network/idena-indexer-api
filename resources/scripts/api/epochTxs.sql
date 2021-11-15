7e72c99d32bb4ad01ec6d607d73ab45c22a31e58
mbidenaio <mb@idena.io>
2021-11-15 23:22:33 +0500

    Update query

--- resources/scripts/api/epochTxs.sql
+++ resources/scripts/api/epochTxs.sql
@@ -20,17 +20,19 @@ select t.id,
        tr.method                                                                                    tx_receipt_method,
        tr.error_msg                                                                                 tx_receipt_error_msg
 from transactions t
-         join blocks b on b.height = t.block_height and b.epoch = $1
-         join addresses afrom on afrom.id = t.from
-         left join addresses ato on ato.id = t.to
-         join dic_tx_types dtt on dtt.id = t.Type
-         left join activation_tx_transfers atxs on atxs.tx_id = t.id and t.type = 1
-         left join kill_tx_transfers ktxs on ktxs.tx_id = t.id and t.type = 3
-         left join kill_invitee_tx_transfers kitxs on kitxs.tx_id = t.id and t.type = 10
-         left join become_online_txs online on online.tx_id = t.id and t.type = 9
-         left join become_offline_txs offline on offline.tx_id = t.id and t.type = 9
+         LEFT JOIN blocks b on b.height = t.block_height
+         LEFT JOIN addresses afrom on afrom.id = t.from
+         LEFT JOIN addresses ato on ato.id = t.to
+         LEFT JOIN dic_tx_types dtt on dtt.id = t.Type
+         LEFT JOIN activation_tx_transfers atxs on atxs.tx_id = t.id and t.type = 1
+         LEFT JOIN kill_tx_transfers ktxs on ktxs.tx_id = t.id and t.type = 3
+         LEFT JOIN kill_invitee_tx_transfers kitxs on kitxs.tx_id = t.id and t.type = 10
+         LEFT JOIN become_online_txs online on online.tx_id = t.id and t.type = 9
+         LEFT JOIN become_offline_txs offline on offline.tx_id = t.id and t.type = 9
          LEFT JOIN tx_receipts tr on t.type in (15, 16, 17) and tr.tx_id = t.id
-WHERE $3::bigint IS NULL
-   OR t.id <= $3
+WHERE t.id <= (SELECT max_tx_id FROM epoch_summaries WHERE epoch = $1)
+  AND t.id >= (SELECT min_tx_id FROM epoch_summaries WHERE epoch = $1)
+  AND ($3::bigint IS NULL
+    OR t.id <= $3)
 order by t.id desc
 limit $2
