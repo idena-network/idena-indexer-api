15fc5a41ff8fbac660933623178651cc74b9fe2c
mbidenaio <mb@idena.io>
2023-05-05 13:52:33 +0500

    Do not show pool history items with zero size

--- resources/scripts/api/poolSizeHistory.sql
+++ resources/scripts/api/poolSizeHistory.sql
@@ -7,5 +7,7 @@ FROM pool_size_history psh
                    ON prev_psh.epoch = psh.epoch - 1 AND prev_psh.address_id = psh.address_id
 WHERE psh.address_id = (SELECT id FROM addresses WHERE lower(address) = lower($1))
   AND ($3::bigint IS NULL OR psh.epoch <= $3)
+  AND psh.validation_size > 0
+  AND coalesce(prev_psh.end_size, 0) > 0
 ORDER BY psh.epoch DESC
 LIMIT $2
